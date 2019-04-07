c48621917c869d3708ecdee66e6e981d14129ddb
ibuky@VTX0 <ivuki.maxx@gmail.com>
2019-04-07 22:07:33 +0900

    new constant var management

---
+++ const.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    IPADDR_DAYTIME  : '192.168.11.51',
+    IPADDR_NIGHT    : '192.168.11.52',
+});
--- cron.js
+++ cron.js
@@ -1,3 +1,5 @@
+const Const = require('./const.js');
+
 const cron = require('node-cron');
 const { Client } = require('tplink-smarthome-api');
 
@@ -13,19 +15,14 @@ const { Client } = require('tplink-smarthome-api');
 
 const client = new Client();
 
-const host = {
-    daytime : '192.168.11.51',  // 日中用
-    night   : '192.168.11.52',  // 夜間用
-}
-
 // 日中
 cron.schedule('0 0 11 * * *', () => {
-    client.getDevice({host: host.daytime}).then((dev) => dev.setPowerState(true));
-    client.getDevice({host: host.night}).then((dev) => dev.setPowerState(false));
+    client.getDevice({host: Const.IPADDR_DAYTIME}).then((dev) => dev.setPowerState(true));
+    client.getDevice({host: Const.IPADDR_NIGHT}).then((dev) => dev.setPowerState(false));
 });
 
 // 夜間
 cron.schedule('0 30 23 * * *', () => {
-    client.getDevice({host: host.daytime}).then((dev) => dev.setPowerState(false));
-    client.getDevice({host: host.night}).then((dev) => dev.setPowerState(true));
+    client.getDevice({host: Const.IPADDR_DAYTIME}).then((dev) => dev.setPowerState(false));
+    client.getDevice({host: Const.IPADDR_NIGHT}).then((dev) => dev.setPowerState(true));
 });
