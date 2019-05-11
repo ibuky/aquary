0ee30a2636ef1592a60287cf3af17dc9b6affe15
ibuky@VTX0 <ivuki.maxx@gmail.com>
2019-05-11 22:59:23 +0900

    change string

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send("OK");
+    res.send("server online");
 });
 
 module.exports = router;
